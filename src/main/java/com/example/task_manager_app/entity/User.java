4338ba7b017628b358d1de4a722ab67e9787d068
Shine-23 <priyadarshine.2322@gmail.com>
2025-05-19 12:18:29 +0530

    Create User.java

---
+++ src/main/java/com/example/task_manager_app/entity/User.java
@@ -0,0 +1,37 @@
+package com.example.task_manager_app.entity;
+
+//represents the person using the app
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.util.Set;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Table(name = "users") // 'user' is a reserved keyword in MySQL
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true, nullable = false)
+    private String username;
+
+    @Column(nullable = false)
+    private String password;
+
+    @ManyToMany(fetch =FetchType.EAGER) //Load the relationship immediately along with the main entity.
+    @JoinTable(
+            name = "user_roles",
+            joinColumns = @JoinColumn(name = "user_id"),
+            inverseJoinColumns = @JoinColumn(name = "role_id")
+    )
+    private Set<Role> roles;
+
+}
