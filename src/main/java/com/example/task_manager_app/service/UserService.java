b779688ea9f3437f79bbbe8cf08c9dae30244b25
Shine-23 <priyadarshine.2322@gmail.com>
2025-05-19 13:30:14 +0530

    Create UserService.java

---
+++ src/main/java/com/example/task_manager_app/service/UserService.java
@@ -0,0 +1,51 @@
+package com.example.task_manager_app.service;
+
+import com.example.task_manager_app.entity.Role;
+import com.example.task_manager_app.entity.User;
+import com.example.task_manager_app.repository.UserRepository;
+import jakarta.persistence.Entity;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import java.util.HashSet;
+import java.util.Optional;
+import java.util.Set;
+
+@Service
+public class UserService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Autowired
+    private RoleService roleService;
+
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
+    public User registerUser(String username, String password, Set<String> roleNames){
+        if(userRepository.existsByUsername(username)){
+            throw new RuntimeException("Username already exists");
+        }
+
+        User newUser = new User();
+        newUser.setUsername(username);
+        newUser.setPassword(passwordEncoder.encode(password));
+
+        Set<Role> roles = new HashSet<>();
+        for(String roleName:roleNames){
+            Role role = roleService.findByName(roleName)
+                    .orElseThrow( () -> new RuntimeException("Role not found: "+ roleName));
+            roles.add(role);
+        }
+
+        newUser.setRoles(roles);
+
+        return userRepository.save(newUser);
+    }
+
+    public Optional<User> findByUsername(String username){
+        return userRepository.findByUsername(username);
+    }
+}
