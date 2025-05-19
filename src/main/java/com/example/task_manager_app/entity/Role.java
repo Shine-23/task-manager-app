1c566997dbea086ce2118ffd9c05749097cc4fe1
Shine-23 <priyadarshine.2322@gmail.com>
2025-05-19 12:17:46 +0530

    Create Role.java

---
+++ src/main/java/com/example/task_manager_app/entity/Role.java
@@ -0,0 +1,22 @@
+package com.example.task_manager_app.entity;
+
+//defines access (e.g., USER, ADMIN)
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Role {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true, nullable = false)
+    private String name;
+}
