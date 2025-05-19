af9425b0089dc8d613cdf62b98902c9bec17b7ed
Shine-23 <priyadarshine.2322@gmail.com>
2025-05-19 14:55:06 +0530

    Create config

--- src/main/java/com/example/task_manager_app/config/SecurityConfig.java
+++ src/main/java/com/example/task_manager_app/config/SecurityConfig.java
@@ -1,7 +1,9 @@
-package com.example.task_manager_app.security;
+package com.example.task_manager_app.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
@@ -11,5 +13,10 @@ public class SecurityConfig {
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration config) throws Exception {
+        return config.getAuthenticationManager();
     }
 }
