a9faa8d9b624754eff561a9aff0212000b3fb86e
Shine-23 <priyadarshine.2322@gmail.com>
2025-05-19 14:35:15 +0530

    Update Role.java
    added getter and setters

--- src/main/java/com/example/task_manager_app/entity/Role.java
+++ src/main/java/com/example/task_manager_app/entity/Role.java
@@ -14,4 +14,19 @@ public class Role {
     @Column(unique = true, nullable = false)
     private String name;
 
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
 }
