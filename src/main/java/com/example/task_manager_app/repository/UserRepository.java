64e93f916d182b940548f5646b01e1ba51c719bf
Shine-23 <priyadarshine.2322@gmail.com>
2025-06-04 12:57:14 +0530

    Update UserRepository.java

--- src/main/java/com/example/task_manager_app/repository/UserRepository.java
+++ src/main/java/com/example/task_manager_app/repository/UserRepository.java
@@ -8,7 +8,7 @@ import java.util.Optional;
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
 
-    Optional<User> findByUsername(String username); // Used for login, signup checks, and fetching user info.
+    Optional<User> findByUsername(String username);
     boolean existsByUsername(String username);
     boolean existsByEmail(String email);
 }
