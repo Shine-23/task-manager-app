84ecc329c2b7a91a442e8f786dc7a2b41aaa1de1
Shine-23 <priyadarshine.2322@gmail.com>
2025-06-05 20:10:25 +0530

    Create CustomUserDetails.java

---
+++ src/main/java/com/example/task_manager_app/security/CustomUserDetails.java
@@ -0,0 +1,53 @@
+package com.example.task_manager_app.security;
+
+import com.example.task_manager_app.entity.User;
+import com.example.task_manager_app.entity.Role;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+public class CustomUserDetails implements UserDetails {
+
+    private final Long id;
+    private final String username;
+    private final String password;
+    private final String email;
+    private final Set<GrantedAuthority> authorities;
+
+    public CustomUserDetails(User user) {
+        this.id = user.getId();
+        this.username = user.getUsername();
+        this.password = user.getPassword();
+        this.email = user.getEmail();
+        this.authorities = user.getRoles().stream()
+                .map(role -> (GrantedAuthority) () -> role.getName().name())
+                .collect(Collectors.toSet());
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return authorities;
+    }
+
+    @Override
+    public String getPassword() {
+        return password;
+    }
+
+    @Override
+    public String getUsername() {
+        return username;
+    }
+
+}
