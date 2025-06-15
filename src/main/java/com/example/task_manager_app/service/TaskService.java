7499af12b3f5067a111133f01a3336c836690dc1
Shine-23 <priyadarshine.2322@gmail.com>
2025-06-15 23:18:03 +0530

    Update TaskService.java

--- src/main/java/com/example/task_manager_app/service/TaskService.java
+++ src/main/java/com/example/task_manager_app/service/TaskService.java
@@ -12,16 +12,19 @@ import java.util.Optional;
 
 public interface TaskService {
     Task saveTask(Task task);
-    Page<Task> findByProject(Project project, Pageable pageable);
 
+
+    Page<Task> findByProject(Project project, Pageable pageable);
 
     Page<Task> findByAssignedUser(User user, Pageable pageable);
 
     Page<Task> findByStatus(TaskStatus status, Pageable pageable);
+
     Page<Task> findByDueDateBefore(LocalDate date, Pageable pageable);
+
     Optional<Task> findTaskById(Long id);
     Optional<Task> updateTask(Task task);
-    void deleteTask(Long id);
+    void deleteTaskById(Long id);
     Page<Task> findTasksByProjectOwner(User owner, Pageable pageable);
 
     long countTasksAssignedToUser(User user);
