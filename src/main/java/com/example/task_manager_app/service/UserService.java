abf43b4ca0796bff44a2e883e0809e1e0f957345
Shine-23 <priyadarshine.2322@gmail.com>
2025-06-15 23:18:09 +0530

    Update UserService.java

--- src/main/java/com/example/task_manager_app/service/UserService.java
+++ src/main/java/com/example/task_manager_app/service/UserService.java
@@ -1,18 +1,29 @@
 package com.example.task_manager_app.service;
 
+import com.example.task_manager_app.dto.UserDTO;
+import com.example.task_manager_app.dto.UserWithProjectDTO;
 import com.example.task_manager_app.entity.User;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 
+import java.util.List;
 import java.util.Optional;
 
-public interface    UserService {
+public interface UserService {
     User saveUser(User user);
     Optional<User> findByUsername(String username);
     boolean existsByUsername(String username);
     boolean existsByEmail(String email);
     Optional<User> findById(Long id);
-    Page<User> getAllUsers(Pageable pageable);
+    Page<UserDTO> getAllUsers(Pageable pageable);
     Optional<User> updateUser(Long id, User user);
     void deleteUser(Long id);
+
+    Page<UserDTO> getUsersByRole(Pageable pageable);
+
+    Page<UserDTO> getUnassignedUsers(Pageable pageable);
+
+    List<User> findUsersByIds(List<Long> ids);
+
+    Page<UserWithProjectDTO> getUsersWithProjectName(Pageable pageable);
 }
